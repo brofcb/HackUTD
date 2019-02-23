e1f0337ea1db21561f666d2a5849bcbd9c27e6cc
brofcb <33383212+brofcb@users.noreply.github.com>
2019-02-23 13:39:36 -0600

    FirstCommit

--- app/src/main/java/com/ibrahim/streetwise/MainActivity.java
+++ app/src/main/java/com/ibrahim/streetwise/MainActivity.java
@@ -5,10 +5,14 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity
 {
 
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 1000;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -24,5 +28,17 @@ public class MainActivity extends AppCompatActivity
                 finish();
             }
         }, SPLASH_TIME_OUT);
+        mAuth = FirebaseAuth.getInstance();
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
 }
